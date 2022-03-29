e2e22389eeb19b69efd98a85c20878a20b3e8e36
Daxton Wirth <66894542+daxtonwirth@users.noreply.github.com>
2022-03-29 18:49:49 +0900

    Update info.ps1

--- info.ps1
+++ info.ps1
@@ -7,8 +7,8 @@ Get-NetIPaddress | sort ifIndex | Select-Object ifIndex, IPAddress, InterfaceAli
 "PORTS"
 Get-NetTcpConnection | sort LocalPort | Group-Object LocalPort
 
-"USERS (consider using get-aduser for domain users)(CMD: net user)"
-Get-LocalUser
+"ACTIVE USERS (consider using get-aduser for domain users)(CMD: net user)"
+Get-LocalUser | ? {$_.enabled -eq "True"}
 
 "SMBv1"
 Get-WindowsOptionalFeature -Online -FeatureName SMB1Protocol | Select-Object State
@@ -25,3 +25,10 @@ Get-NetNeighbor | sort IPAddress | % {if (!($_.IPAddress -match '.255') -and !($
 
 "Scheduled tasks"
 Get-ScheduledTask | Sort-Object State , TaskName | % {if ($_.state -ne "Disabled") {$_}}
+
+
+"Running Services (alternative: net start)"
+Get-Service | ? {$_.Status -eq "Running"} | sort Name
+
+"Non-windows services"
+Get-wmiobject win32_service | where { $_.Caption -notmatch "Windows" -and $_.PathName -notmatch "Windows" -and $_.PathName -notmatch "policyhost.exe" -and $_.Name -ne "LSM" -and $_.PathName -notmatch "OSE.EXE" -and $_.PathName -notmatch "OSPPSVC.EXE" -and $_.PathName -notmatch "Microsoft Security Client" } | ft
