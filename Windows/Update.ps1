c53830eb5cc2f02198ab8467b1ce9f8263d258dd
Daxton Wirth <66894542+daxtonwirth@users.noreply.github.com>
2022-03-29 22:18:33 +0900

    Update Update.ps1

--- Windows/Update.ps1
+++ Windows/Update.ps1
@@ -1,21 +1,22 @@
 # https://docs.microsoft.com/en-us/powershell/scripting/learn/ps101/08-powershell-remoting?view=powershell-7.2 
-# One-To-One Remoting: 
-# WinRM
-# Enter-PSSession COMPUTERNAME
-# Enable-PSRemoting # Winrm
-# One-To-Many Remoting is invoke-command
-
-# Get-Command -ParameterName ComputerName
-
+# One-To-One Remoting: WinRM (Enable-PSRemoting # Winrm; Enter-PSSession COMPUTERNAME)
+# One-To-Many Remoting: invoke-command
+# Commands that have remoting built in: Get-Command -ParameterName ComputerName
+# If running slow, reduce the overhead with sessions: $Cred = Get-Credential; $Session = New-PSSession -ComputerName dc01, sql02, web01 -Credential $Cred
 
 $COMPUTERS = Get-ADComputer -Filter * | % {$_.name} 
 
-Invoke-Command -ComputerName $COMPUTERS -ScriptBlock {.\update.ps1}
+
+Invoke-Command -ComputerName $COMPUTERS -ScriptBlock {
+
 
 Disable-PSRemoting
 stop-service winrm
+
 start-service Windefend
 Update-MpSignature
+Set-NetFirewallProfile -Profile Domain, Public, Private -Enabled True
+
 # Updates
 wuauclt /detectnow /updatenow
 
@@ -23,9 +24,7 @@ wuauclt /detectnow /updatenow
 net user Guest /active:NO
 # net user Administrator /active:NO
 
+Disable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol
 
 
-## Other userful commmands:
-* Reduce overhead:
-$Session = New-PSSession -ComputerName dc01, sql02, web01 -Credential $Cred
-$Cred = Get-Credential
+}
