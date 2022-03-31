0359b2fb738843458f0064d4a36eb0dbbce6d2b7
Daxton Wirth <66894542+daxtonwirth@users.noreply.github.com>
2022-03-31 21:27:30 +0900

    Update domain-info.ps1

--- Windows/domain-info.ps1
+++ Windows/domain-info.ps1
@@ -1,18 +1,24 @@
+"-----------------------------------------------------------------------------------------------"
 "DOMAIN NAME"
 Get-ADDomain | Select-Object Name, dnsroot, userscontainer
 
+"-----------------------------------------------------------------------------------------------"
 "DC"
 Get-ADDomainController | Select-Object Name, OperatingSystem, ldapport, ipv4address
 
+"-----------------------------------------------------------------------------------------------"
 "Group info"
 get-adgroup -Filter * | Select-Object Name, GroupScope, GroupCategory, DistinguishedName | sort name |ft
 
+"-----------------------------------------------------------------------------------------------"
 "GPO"
 get-gpo -all | Select-Object displayname, domainname, owner,gpostatus, description, id | ft
 
+"-----------------------------------------------------------------------------------------------"
 "ADMINS"
 Get-ADGroupMember administrators | Select-Object Name, objectclass, samaccountname | ft
 
+"-----------------------------------------------------------------------------------------------"
 "Users"
 get-aduser -Filter * | sort name | Select-Object Name, enabled, objectclass, DistinguishedName
-
+"-----------------------------------------------------------------------------------------------"
